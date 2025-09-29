9559e0276445b35c0130595412d9d47162862978
AyrtonBLD <ayrton.blondeau@ynov.com>
2025-09-29 14:03:25 +0200

    Fix de Evan

--- grille1.go
+++ grille1.go
@@ -1,16 +1,18 @@
 package main
 
+import "fmt"
+
 func displayBoard(game *Game) {
-    fmt.Println("\nÉtat actuel de la grille :")
-    for col := 0; col < columns; col++ {
-        fmt.Printf(" %d", col)
-    }
-    fmt.Println()
-    for , row := range game.board {
-        for , cell := range row {
-            fmt.Printf(" %c", cell)
-        }
-        fmt.Println()
-    }
-    fmt.Println()
+	fmt.Println("\nÉtat actuel de la grille :")
+	for col := 0; col < columns; col++ {
+		fmt.Printf(" %d", col)
+	}
+	fmt.Println()
+	for _, row := range game.board {
+		for _, cell := range row {
+			fmt.Printf(" %c", cell)
+		}
+		fmt.Println()
+	}
+	fmt.Println()
 }
